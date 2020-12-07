cd45853bcb29777775f1ba4e2746304a76029fc1
Ankit Punia <55357734+ankit0tech@users.noreply.github.com>
2020-12-07 22:53:30 +0530

    Add files via upload

---
+++ models/admin/Admin.php
@@ -0,0 +1,38 @@
+<?php
+
+class Admin
+{
+    private $id;
+    private $name;
+    private $pass;
+    private static $admins=[];
+
+
+   // Admin constructor  $id  $name $pass
+
+    public function __construct ($id, $name, $pass)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->pass = $pass;
+        Admin::$admins;
+
+    }
+
+
+      // @return mixed
+
+    public function getId ()
+    {
+        return $this->id;
+    }
+    public function getName ()
+    {
+        return $this->name;
+    }
+    public function getPass ()
+    {
+        return $this->pass;
+    }
+
+}
---
+++ models/goods/Address.php
@@ -0,0 +1,64 @@
+<?php
+
+
+spl_autoload_register(function ($class){
+    $arr=['goods','interfaces','orders','reviews','serve'];
+    foreach ($arr as $val) {
+        $path=__DIR__."/../$val/$class.php";
+        if (file_exists($path))
+            require_once $path;
+    }
+});
+
+class Address
+{
+    private $country;
+    private $city;
+    private $street;
+    private $buildNo;
+
+
+     // Address constructor $country $city $street $buildNo
+
+    public function __construct($country, $city, $street, $buildNo)
+    {
+        $this->country = $country;
+        $this->city = $city;
+        $this->street = $street;
+        $this->buildNo = $buildNo;
+    }
+
+    // @return mixed
+    public function getCountry ()
+    {
+        return $this->country;
+    }
+
+
+      // @return mixed
+    public function getCity ()
+    {
+        return $this->city;
+    }
+
+    public function getStreet ()
+    {
+        return $this->street;
+    }
+
+    public function getBuildNo ()
+    {
+        return $this->buildNo;
+    }
+
+
+    public  function __toString ()
+    {
+        return<<<EOD
+the country is $this->country , city is $this->city, street is $this->street and build NO is
+$this->buildNo
+EOD;
+
+    }
+
+}
